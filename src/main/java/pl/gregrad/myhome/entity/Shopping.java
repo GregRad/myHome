1f189e93353d15f7e085fb58771409eacc936a66
GregRad <radles23@gmail.com>
2019-01-27 22:17:51 +0100

    add entity

--- src/main/java/pl/gregrad/myhome/entity/Shopping.java
+++ src/main/java/pl/gregrad/myhome/entity/Shopping.java
@@ -1,4 +1,16 @@
 package pl.gregrad.myhome.entity;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Shopping")
 public class Shopping {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer price;
+    private LocalDateTime date;
+
 }
--- src/main/java/pl/gregrad/myhome/entity/Statistics.java
+++ src/main/java/pl/gregrad/myhome/entity/Statistics.java
@@ -1,4 +1,9 @@
 package pl.gregrad.myhome.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Statistics")
 public class Statistics {
 }
