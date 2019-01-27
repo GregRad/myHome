9dfabfb5d6481520eac56dc06dba6d06db180fd2
GregRad <radles23@gmail.com>
2019-01-27 22:17:40 +0100

    add entity

--- src/main/java/pl/gregrad/myhome/entity/Bills.java
+++ src/main/java/pl/gregrad/myhome/entity/Bills.java
@@ -1,5 +1,48 @@
 package pl.gregrad.myhome.entity;
 
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Bills")
 public class Bills {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String value;
+    private LocalDateTime paymentDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDateTime paymentDate) {
+        this.paymentDate = paymentDate;
+    }
 }
--- src/main/java/pl/gregrad/myhome/entity/Consumption.java
+++ src/main/java/pl/gregrad/myhome/entity/Consumption.java
@@ -1,4 +1,37 @@
 package pl.gregrad.myhome.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Consumption")
 public class Consumption {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
