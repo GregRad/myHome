3a0d2a9e73fde5ac4d2ed529b105710278a174e8
GregRad <radles23@gmail.com>
2019-01-29 22:47:05 +0100

    add service layer

---
+++ src/main/java/pl/gregrad/myhome/services/BillsService.java
@@ -0,0 +1,10 @@
+package pl.gregrad.myhome.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class BillsService {
+}
---
+++ src/main/java/pl/gregrad/myhome/services/ConsumptionService.java
@@ -0,0 +1,11 @@
+package pl.gregrad.myhome.services;
+
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ConsumptionService {
+}
