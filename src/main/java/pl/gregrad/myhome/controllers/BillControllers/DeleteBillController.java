db22c238e78431483f984cb45c525db403da7e71
GregRad <radles23@gmail.com>
2019-02-02 22:23:25 +0100

    edit controllers

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/DeleteBillController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/DeleteBillController.java
@@ -1,4 +1,9 @@
 package pl.gregrad.myhome.controllers.BillControllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class DeleteBillController {
 }
--- src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/EditConsumptionController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/EditConsumptionController.java
@@ -1,4 +1,9 @@
 package pl.gregrad.myhome.controllers.ConsumptionControllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class EditConsumptionController {
 }
