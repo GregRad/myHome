f00b6d2482b4a9d900e151ceb0848f87166f9c6a
GregRad <radles23@gmail.com>
2019-02-06 21:31:43 +0100

    rework controller

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/AddBillController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/AddBillController.java
@@ -1,9 +1,15 @@
 package pl.gregrad.myhome.controllers.BillControllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class AddBillController {
+
+    @GetMapping
+    public String billsForm() {
+        return "Add_Bill";
+    }
 }
