a7097879af5d61ceaa730baab814eeb9089f9015
GregRad <radles23@gmail.com>
2019-02-07 21:25:20 +0100

    addBill method

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/AddBillController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/AddBillController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/bills/addBill")
 public class AddBillController {
 
     @GetMapping
-    public String billsForm() {
+    public String addBillForm() {
         return "Add_Bill";
     }
 }
