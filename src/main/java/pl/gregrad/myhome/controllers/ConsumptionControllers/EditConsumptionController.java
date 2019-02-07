4d3eb1207235b9ae69cbc74e47273a9d010882d8
GregRad <radles23@gmail.com>
2019-02-07 21:45:05 +0100

    add mapping address

--- src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/EditConsumptionController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/EditConsumptionController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("consumption/editConsumption")
+@RequestMapping("/consumption/editConsumption")
 public class EditConsumptionController {
 
     public String editConsumption() {
