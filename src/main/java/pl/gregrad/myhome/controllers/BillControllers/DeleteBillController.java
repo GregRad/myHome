abbcb95db230fc600b5411d7a5223051f89fb878
GregRad <radles23@gmail.com>
2019-02-07 21:25:36 +0100

    deleteBill method

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/DeleteBillController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/DeleteBillController.java
@@ -1,9 +1,15 @@
 package pl.gregrad.myhome.controllers.BillControllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("bills/deleteBill")
 public class DeleteBillController {
+
+    @GetMapping
+    public String deleteBill() {
+        return "redirect:/bills";
+    }
 }
