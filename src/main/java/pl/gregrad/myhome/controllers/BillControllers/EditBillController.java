6a166777036a0c762b2de1bdfb8bce143ad06e9f
GregRad <radles23@gmail.com>
2019-02-07 21:25:58 +0100

    editBill controler method

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/EditBillController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/EditBillController.java
@@ -1,9 +1,15 @@
 package pl.gregrad.myhome.controllers.BillControllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class EditBillController {
+
+    @GetMapping
+    public String editBillForm() {
+        return "Edit_Bill";
+    }
 }
