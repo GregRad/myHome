4d38b146009b95ffc79b031a125291bc26b0bfc1
GregRad <radles23@gmail.com>
2019-02-09 00:56:16 +0100

    add consumption service

--- src/main/java/pl/gregrad/myhome/services/ConsumptionService.java
+++ src/main/java/pl/gregrad/myhome/services/ConsumptionService.java
@@ -1,11 +1,43 @@
 package pl.gregrad.myhome.services;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.gregrad.myhome.dto.ConsumptionDTO;
+import pl.gregrad.myhome.entity.Consumption;
+import pl.gregrad.myhome.repositories.ConsumptionRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 public class ConsumptionService {
+
+    @Autowired
+    private ConsumptionRepository consumptionRepository;
+
+    public List<ConsumptionDTO> findAllConsumption() {
+        List<Consumption> consumption = consumptionRepository.findAll();
+        List<ConsumptionDTO> allConsumption = new ArrayList<>();
+        for (Consumption c : consumption) {
+            ConsumptionDTO consumptionAll = new ConsumptionDTO();
+            consumptionAll.setName(c.getName());
+            consumptionAll.setValue(c.getValue());
+            allConsumption.add(consumptionAll);
+        }
+        return allConsumption;
+    }
+
+    public void deleteConsumption(Long id) {
+        consumptionRepository.delete(consumptionRepository.findOne(id));
+    }
+
+    public void editConsumption(ConsumptionDTO consumption) {
+        Consumption editConsumption = consumptionRepository.findOne(consumption.getId());
+        editConsumption.setName(consumption.getName());
+        editConsumption.setValue(consumption.getValue());
+        consumptionRepository.save(editConsumption);
+    }
 }
