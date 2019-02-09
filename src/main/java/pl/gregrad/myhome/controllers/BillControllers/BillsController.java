3d203cddd46bd9ab2fc8a82e349a34ab39d37bcc
GregRad <radles23@gmail.com>
2019-02-09 21:35:30 +0100

    add method to show all bills

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/BillsController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/BillsController.java
@@ -1,16 +1,27 @@
 package pl.gregrad.myhome.controllers.BillControllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gregrad.myhome.dto.BillsDTO;
+import pl.gregrad.myhome.services.BillsService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/bills")
 public class BillsController {
 
-    @GetMapping
-    public String bills() {
+    @Autowired
+    private BillsService billsService;
+
+    @GetMapping("/allBills")
+    public String bills(Model model) {
+        List<BillsDTO> allBills = billsService.findAllBills();
+        model.addAttribute("bills", allBills);
         return "Bills";
     }
 }
