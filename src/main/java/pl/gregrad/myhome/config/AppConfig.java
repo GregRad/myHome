5be06ec6e26eff4a9a3cc9ca85c835e7baccf4db
GregRad <radles23@gmail.com>
2019-02-09 21:34:05 +0100

    add jpa transactional annotation

--- src/main/java/pl/gregrad/myhome/config/AppConfig.java
+++ src/main/java/pl/gregrad/myhome/config/AppConfig.java
@@ -3,8 +3,10 @@ package pl.gregrad.myhome.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -16,6 +18,8 @@ import javax.persistence.EntityManagerFactory;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.gregrad.myhome")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "pl.gregrad.myhome.repositories")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Bean
