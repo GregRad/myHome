7491bc65a0d1d279735a3e7aa6df7ed469aa06b2
GregRad <radles23@gmail.com>
2019-02-12 23:28:40 +0100

    add edit bill controller

--- src/main/java/pl/gregrad/myhome/controllers/BillControllers/EditBillController.java
+++ src/main/java/pl/gregrad/myhome/controllers/BillControllers/EditBillController.java
@@ -1,15 +1,29 @@
 package pl.gregrad.myhome.controllers.BillControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.gregrad.myhome.dto.BillsDTO;
+import pl.gregrad.myhome.services.BillsService;
 
 @Controller
-@RequestMapping("/bills/editBill")
+@RequestMapping("/bills")
 public class EditBillController {
 
-    @GetMapping
-    public String editBillForm() {
+    @Autowired
+    private BillsService billsService;
+
+    @GetMapping("/edit/{id}")
+    public String editBillForm(@PathVariable Long id, Model model) {
+    model.addAttribute("editBill", billsService.findBill(id));
         return "Edit_Bill";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editBill(@ModelAttribute BillsDTO editBill) {
+        billsService.edit(editBill);
+        return "redirect:/bills/all_Bills";
+
     }
 }
