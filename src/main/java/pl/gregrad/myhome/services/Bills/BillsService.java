a87116fe3ae3ea898c1e44c96a0c69b72a417ab0
GregRad <radles23@gmail.com>
2019-02-13 23:24:54 +0100

    update bill service

--- src/main/java/pl/gregrad/myhome/services/Bills/BillsService.java
+++ src/main/java/pl/gregrad/myhome/services/Bills/BillsService.java
@@ -1,4 +1,4 @@
-package pl.gregrad.myhome.services;
+package pl.gregrad.myhome.services.Bills;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
