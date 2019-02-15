f936edb28119f09f7f76be3003325c37126371db
GregRad <radles23@gmail.com>
2019-02-15 23:34:55 +0100

    new controller

--- src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/ConsumptionController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/ConsumptionController.java
@@ -1,16 +1,23 @@
 package pl.gregrad.myhome.controllers.ConsumptionControllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gregrad.myhome.services.Consumption.ConsumptionService;
 
 @Controller
 @RequestMapping("/consumption")
 public class ConsumptionController {
 
-    @GetMapping
-    public String consumption() {
+    @Autowired
+    private ConsumptionService consumptionService;
+
+    @GetMapping("/all_Consumptions")
+    public String allConsumption() {
+        consumptionService.findAllConsumptions();
         return "Consumption";
     }
+
 }
