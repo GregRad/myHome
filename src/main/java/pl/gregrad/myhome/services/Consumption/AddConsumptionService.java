ac9d2ba8e9fad12d5dfc882da78b2f573e2470da
GregRad <radles23@gmail.com>
2019-02-15 23:36:13 +0100

    update consumptions service

--- src/main/java/pl/gregrad/myhome/services/Consumption/AddConsumptionService.java
+++ src/main/java/pl/gregrad/myhome/services/Consumption/AddConsumptionService.java
@@ -1,13 +1,28 @@
 package pl.gregrad.myhome.services.Consumption;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.gregrad.myhome.dto.ConsumptionDTO;
+import pl.gregrad.myhome.entity.Consumption;
+import pl.gregrad.myhome.repositories.ConsumptionRepository;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
 public class AddConsumptionService {
+
+    @Autowired
+    private ConsumptionRepository consumptionRepository;
+
+    public void addConsumption(ConsumptionDTO consumptionForm) {
+        Consumption consumption = new Consumption();
+        consumption.setName(consumptionForm.getName());
+        consumption.setValue(consumptionForm.getValue());
+        consumptionRepository.save(consumption);
+
+    }
 
 
 
