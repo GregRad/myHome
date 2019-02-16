7e598822d2e63710a3f77b2e3d890ca013edf5b6
GregRad <radles23@gmail.com>
2019-02-16 23:19:23 +0100

    new consumption controller methods

--- src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/AddConsumptionController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/AddConsumptionController.java
@@ -1,14 +1,32 @@
 package pl.gregrad.myhome.controllers.ConsumptionControllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gregrad.myhome.dto.ConsumptionDTO;
+import pl.gregrad.myhome.services.Consumption.AddConsumptionService;
+import pl.gregrad.myhome.services.Consumption.ConsumptionService;
 
 @Controller
-@RequestMapping("/consumption/addConsumption")
+@RequestMapping("/consumption")
 public class AddConsumptionController {
 
-    public String addConsumption() {
+    @Autowired
+    private AddConsumptionService addConsumptionService;
+
+    @GetMapping("/add_Consumption")
+    public String showConsumptionForm(Model model) {
+        model.addAttribute("consumptionForm", new ConsumptionDTO());
         return "Add_Consumption";
+    }
+    @PostMapping("/add_Consumption")
+    public String addConsumption(@ModelAttribute ConsumptionDTO consumption) {
+        addConsumptionService.addConsumption(consumption);
+        return "redirect:/consumption/all_Consumptions";
     }
 }
--- src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/ConsumptionController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/ConsumptionController.java
@@ -3,9 +3,13 @@ package pl.gregrad.myhome.controllers.ConsumptionControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gregrad.myhome.dto.ConsumptionDTO;
 import pl.gregrad.myhome.services.Consumption.ConsumptionService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/consumption")
@@ -15,9 +19,10 @@ public class ConsumptionController {
     private ConsumptionService consumptionService;
 
     @GetMapping("/all_Consumptions")
-    public String allConsumption() {
-        consumptionService.findAllConsumptions();
-        return "Consumption";
+    public String consumptions(Model model) {
+        List<ConsumptionDTO> allConsumptions = consumptionService.findAllConsumptions();
+        model.addAttribute("consumptions", allConsumptions);
+        return "Consumptions";
     }
 
 }
