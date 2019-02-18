fd1e179014709a3368ae0613ce79fcd4babd1548
GregRad <radles23@gmail.com>
2019-02-18 23:42:28 +0100

    add new method to consumption controller

--- src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/EditConsumptionController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ConsumptionControllers/EditConsumptionController.java
@@ -1,13 +1,27 @@
 package pl.gregrad.myhome.controllers.ConsumptionControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.gregrad.myhome.dto.ConsumptionDTO;
+import pl.gregrad.myhome.services.Consumption.ConsumptionService;
 
 @Controller
-@RequestMapping("/consumption/editConsumption")
+@RequestMapping("/consumption")
 public class EditConsumptionController {
 
-    public String editConsumption() {
+    @Autowired
+    private ConsumptionService consumptionService;
+
+    @GetMapping("/editConsumption/{id}")
+    public String editConsumptionForm(@PathVariable Long id, Model model) {
+        model.addAttribute("editConsumption", consumptionService.findConsumption(id));
         return "Edit_Consumption";
+    }
+    @PostMapping("/editConsumption/{id}")
+    public String editConsumption(@ModelAttribute ConsumptionDTO editConsumption){
+        consumptionService.edit(editConsumption);
+        return "redirect:/consumption/all_Consumptions";
     }
 }
