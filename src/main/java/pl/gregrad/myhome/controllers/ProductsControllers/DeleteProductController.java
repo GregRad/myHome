735c4b0ca2b9685e7790d7c60a1cdf0385134287
GregRad <radles23@gmail.com>
2019-03-07 20:23:27 +0100

    add confirm delete functionality

--- src/main/java/pl/gregrad/myhome/controllers/ProductsControllers/DeleteProductController.java
+++ src/main/java/pl/gregrad/myhome/controllers/ProductsControllers/DeleteProductController.java
@@ -2,6 +2,7 @@ package pl.gregrad.myhome.controllers.ProductsControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,12 @@ public class DeleteProductController {
 
     @Autowired
     ProductsService productsService;
+
+    @GetMapping("/confirm/{id}")
+    public String confirm(@PathVariable Long id, Model model){
+        model.addAttribute("product", productsService.findProduct(id));
+        return "Delete_Product";
+    }
 
     @GetMapping("/delete/{id}")
     public String addProduct(@PathVariable Long id) {
