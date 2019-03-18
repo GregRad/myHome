f9652097323c0c4003d9ccec4299368728f466ee
GregRad <radles23@gmail.com>
2019-03-18 23:29:34 +0100

    update query

--- src/main/java/pl/gregrad/myhome/repositories/ProductsRepository.java
+++ src/main/java/pl/gregrad/myhome/repositories/ProductsRepository.java
@@ -1,11 +1,14 @@
 package pl.gregrad.myhome.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.gregrad.myhome.entity.Products;
 
 import java.util.List;
 
 public interface ProductsRepository extends JpaRepository<Products, Long> {
 
-    List<Products> findProductsByCategory (String cateygory);
+    @Query ("SELECT p FROM Products p WHERE CATEGORY(p.category) = :category")
+    List<Products> findProductsByCategory (@Param("category") String category);
 }
