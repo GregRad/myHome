88527459a50234ba741535c4a859565d6d23e7be
GregRad <radles23@gmail.com>
2019-04-01 23:10:39 +0200

    update repository

--- src/main/java/pl/gregrad/myhome/repositories/ProductsRepository.java
+++ src/main/java/pl/gregrad/myhome/repositories/ProductsRepository.java
@@ -15,8 +15,8 @@ public interface ProductsRepository extends JpaRepository<Products, Long> {
     @Query ("SELECT d FROM Products d WHERE MONTH(d.date) = :date")
     List<Products> findProductsByDate (@Param("date") Integer date);
     
-    @Query ("SELECT c FROM Products c WHERE MONTH(c.date) =:date AND c.category = :category")
-    List<Products> findCategorizeProducts (@Param("date") Integer date, @Param("category") String category);
+    @Query ("SELECT c FROM Products c WHERE YEAR(c.date) =:year AND MONTH(c.date) =:date AND c.category = :category")
+    List<Products> findCategorizeProducts (@Param("year") Integer year, @Param("date") Integer date, @Param("category") String category);
     
     @Query ("SELECT y FROM Products y WHERE YEAR(y.date) =:date")
     List<Products> findProductsByYear (@Param("date") Integer date);
