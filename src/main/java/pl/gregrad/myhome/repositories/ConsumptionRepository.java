0c88e0262505b7d9ea36bb397a9ae6ecaa1d5286
GregRad <radles23@gmail.com>
2019-04-04 21:49:19 +0200

    add new query

--- src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
+++ src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
@@ -1,7 +1,17 @@
 package pl.gregrad.myhome.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import pl.gregrad.myhome.entity.Consumption;
 
 public interface ConsumptionRepository extends JpaRepository<Consumption, Long> {
+	
+	@Query("SELECT c FROM Consumption c WHERE c.type = :type")
+	List<Consumption> consumptionsByType (@Param("type") String type);
+	
+	
 }
