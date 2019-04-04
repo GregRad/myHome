0a7c2aaab224171fe62315cbffacf61206c7d8f3
GregRad <radles23@gmail.com>
2019-04-05 01:40:53 +0200

    add new query in consumption repository

--- src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
+++ src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
@@ -10,8 +10,19 @@ import pl.gregrad.myhome.entity.Consumption;
 
 public interface ConsumptionRepository extends JpaRepository<Consumption, Long> {
 	
-	@Query("SELECT c FROM Consumption c WHERE c.type = :type")
-	List<Consumption> consumptionsByType (@Param("type") String type);
+	@Query("SELECT c FROM Consumption c WHERE c.type =:type")
+	List<Consumption> consumptionsByType (@Param("type") String type); 
 	
+	@Query("SELECT c FROM Consumption c WHERE YEAR(c.date) =:year")
+	List<Consumption> consumptionsByYear (@Param("year") Integer year);
 	
+	@Query("SELECT c FROM Consumption c WHERE MONTH(c.date) =: month")
+	List<Consumption> consumptionsByMonth (@Param("month") Integer month);
+	
+	@Query("SELECT c FROM Consumptions c WHERE YEAR(c.date) =:year AND c.type =: type")
+	List<Consumption> consumptionsTypeByYear (@Param("year") Integer year, @Param("type") String type);
+
+	@Query("SELECT c FROM Consumptions c WHERE MONTH(c.date) =:month AND c.type =: type")
+	List<Consumption> consumptionsTypeByMonth (@Param("month") Integer month, @Param("type") String type);
 }
+
