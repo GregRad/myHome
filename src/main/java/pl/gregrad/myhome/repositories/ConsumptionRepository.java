359c19d10b22b99ef7e5e5971eb113c568bffe4b
GregRad <radles23@gmail.com>
2019-04-06 22:23:01 +0200

    new methods in consumption service

--- src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
+++ src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
@@ -11,18 +11,18 @@ import pl.gregrad.myhome.entity.Consumption;
 public interface ConsumptionRepository extends JpaRepository<Consumption, Long> {
 	
 	@Query("SELECT c FROM Consumption c WHERE c.type =:type")
-	List<Consumption> consumptionsByType (@Param("type") String type); 
+	List<Consumption> findConsumptionsByType (@Param("type") String type); 
 	
 	@Query("SELECT c FROM Consumption c WHERE YEAR(c.date) =:year")
-	List<Consumption> consumptionsByYear (@Param("year") Integer year);
+	List<Consumption> findConsumptionsByYear (@Param("year") Integer year);
 	
-	@Query("SELECT c FROM Consumption c WHERE MONTH(c.date) =: month")
-	List<Consumption> consumptionsByMonth (@Param("month") Integer month);
+	@Query("SELECT c FROM Consumption c WHERE MONTH(c.date) =:month")
+	List<Consumption> findConsumptionsByMonth (@Param("month") Integer month);
 	
-	@Query("SELECT c FROM Consumptions c WHERE YEAR(c.date) =:year AND c.type =: type")
-	List<Consumption> consumptionsTypeByYear (@Param("year") Integer year, @Param("type") String type);
+	@Query("SELECT c FROM Consumptions c WHERE YEAR(c.date) =:year AND c.type =:type")
+	List<Consumption> findConsumptionsTypeByYear (@Param("year") Integer year, @Param("type") String type);
 
-	@Query("SELECT c FROM Consumptions c WHERE MONTH(c.date) =:month AND c.type =: type")
-	List<Consumption> consumptionsTypeByMonth (@Param("month") Integer month, @Param("type") String type);
+	@Query("SELECT c FROM Consumptions c WHERE MONTH(c.date) =:month AND c.type =:type")
+	List<Consumption> findConsumptionsTypeByMonth (@Param("month") Integer month, @Param("type") String type);
 }
 
--- src/main/java/pl/gregrad/myhome/services/Consumption/ConsumptionService.java
+++ src/main/java/pl/gregrad/myhome/services/Consumption/ConsumptionService.java
@@ -3,6 +3,8 @@ package pl.gregrad.myhome.services.Consumption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
 import pl.gregrad.myhome.dto.ConsumptionDTO;
 import pl.gregrad.myhome.entity.Consumption;
 import pl.gregrad.myhome.repositories.ConsumptionRepository;
@@ -37,10 +39,79 @@ public class ConsumptionService {
         findConsumption.setId(consumption.getId());
         findConsumption.setName(consumption.getName());
         findConsumption.setValue(consumption.getValue());
-
         return findConsumption;
     }
-
+    public List<ConsumptionDTO> findConsumptionByType (String type) {
+    	List<Consumption> consumptions= consumptionRepository.findConsumptionsByType(type);
+    	List<ConsumptionDTO> findConsumptionByType = new ArrayList<>();
+    	for (Consumption c : consumptions) {
+    		ConsumptionDTO consumptionByType = new ConsumptionDTO();
+    		consumptionByType.setId(c.getId());
+    		consumptionByType.setDate(c.getDate());
+    		consumptionByType.setName(c.getName());
+    		consumptionByType.setType(c.getType());
+    		consumptionByType.setValue(c.getValue());
+    		findConsumptionByType.add(consumptionByType);
+    	}
+    	return findConsumptionByType;
+    }
+    public List<ConsumptionDTO> findConsumptionsByYear (Integer year) {
+    	List<Consumption> consumptions = consumptionRepository.findConsumptionsByYear(year);
+    	List<ConsumptionDTO> findConsumptionsByYear = findConsumptionsByYear(year);
+    	for (Consumption c : consumptions) {
+    		ConsumptionDTO consumptionsByYear = new ConsumptionDTO();
+     		consumptionsByYear.setName(c.getName());
+     		consumptionsByYear.setValue(c.getValue());
+     		consumptionsByYear.setType(c.getType());
+     		consumptionsByYear.setDate(c.getDate());
+     		consumptionsByYear.setId(c.getId());
+     		findConsumptionsByYear.add(consumptionsByYear);
+    	}
+    	return findConsumptionsByYear;
+    }
+    public List<ConsumptionDTO> findConsumptionsByMonth (Integer month) {
+    	List<Consumption> consumptions = consumptionRepository.findConsumptionsByMonth(month);
+    	List<ConsumptionDTO> findConsumptionsByMonth = new ArrayList<ConsumptionDTO>();
+    	for (Consumption c : consumptions) {
+    		ConsumptionDTO consumptionsByMonth = new ConsumptionDTO();
+    		consumptionsByMonth.setName(c.getName());
+    		consumptionsByMonth.setValue(c.getValue());
+    		consumptionsByMonth.setType(c.getType());
+    		consumptionsByMonth.setDate(c.getDate());
+    		consumptionsByMonth.setId(c.getId());
+    		findConsumptionsByMonth.add(consumptionsByMonth);
+    	}
+    	return findConsumptionsByMonth;
+    }
+    public List<ConsumptionDTO> findConsumprionsByYearAndType (Integer year, String type) {
+    	List<Consumption> consumptions = consumptionRepository.findConsumptionsTypeByYear(year, type);
+    	List<ConsumptionDTO> findConsumptionsByYearAndType = new ArrayList<ConsumptionDTO>();
+    	for (Consumption c : consumptions) {
+    		ConsumptionDTO consumptionsByYearAndType = new ConsumptionDTO();
+    		consumptionsByYearAndType.setName(c.getName());
+    		consumptionsByYearAndType.setValue(c.getValue());
+    		consumptionsByYearAndType.setDate(c.getDate());
+    		consumptionsByYearAndType.setId(c.getId());
+    		consumptionsByYearAndType.setType(c.getType());
+    		findConsumptionsByYearAndType.add(consumptionsByYearAndType);
+    	}
+    	return findConsumptionsByYearAndType;
+    }
+    public List<ConsumptionDTO> findConsumprionsByMonthAndType (Integer month, String type) {
+    	List<Consumption> consumptions = consumptionRepository.findConsumptionsTypeByMonth(month, type);
+    	List<ConsumptionDTO> findConsumptionsByMonthAndType = new ArrayList<ConsumptionDTO>();
+    	for (Consumption c : consumptions) {
+    		ConsumptionDTO consumptionsByMonthAndType = new ConsumptionDTO();
+    		consumptionsByMonthAndType.setName(c.getName());
+    		consumptionsByMonthAndType.setValue(c.getValue());
+    		consumptionsByMonthAndType.setDate(c.getDate());
+    		consumptionsByMonthAndType.setId(c.getId());
+    		consumptionsByMonthAndType.setType(c.getType());
+    		findConsumptionsByMonthAndType.add(consumptionsByMonthAndType);
+    	}
+    	return findConsumptionsByMonthAndType;
+    }
+    
     public void delete(Long id) {
         consumptionRepository.delete(consumptionRepository.findOne(id));
     }
