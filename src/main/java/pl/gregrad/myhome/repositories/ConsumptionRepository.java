1f41776c6618a06a580707c0a8a2f38b152404cc
GregRad <radles23@gmail.com>
2019-04-08 23:20:56 +0200

    fix consumption query
    z
    0

--- src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
+++ src/main/java/pl/gregrad/myhome/repositories/ConsumptionRepository.java
@@ -19,10 +19,9 @@ public interface ConsumptionRepository extends JpaRepository<Consumption, Long> 
 	@Query("SELECT c FROM Consumption c WHERE MONTH(c.date) = :month")
 	List<Consumption> findConsumptionsByMonth (@Param("month") Integer month);
 	
-	@Query("SELECT c FROM Consumptions c WHERE YEAR(c.date) = :year AND c.type = :type")
+	@Query("SELECT c FROM Consumption c WHERE YEAR(c.date) = :year AND c.type = :type")
 	List<Consumption> findConsumptionsTypeByYear (@Param("year") Integer year, @Param("type") String type);
 
-	@Query("SELECT c FROM Consumptions c WHERE MONTH(c.date) = :month AND c.type = :type")
+	@Query("SELECT c FROM Consumption c WHERE MONTH(c.date) = :month AND c.type = :type")
 	List<Consumption> findConsumptionsTypeByMonth (@Param("month") Integer month, @Param("type") String type);
 }
-
