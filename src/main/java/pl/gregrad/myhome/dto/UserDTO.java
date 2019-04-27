fd148d2c0663fb719a56164aaa286dd37ed1261f
Greg <radles23@gmail.com>
2019-04-27 23:12:58 +0200

    setup for user and login option

---
+++ src/main/java/pl/gregrad/myhome/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package pl.gregrad.myhome.dto;
+
+public class UserDTO {
+
+		private Integer id;
+		private String name;
+		private String surname;
+		
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getSurname() {
+			return surname;
+		}
+		public void setSurname(String surname) {
+			this.surname = surname;
+		}
+		
+		
+}
